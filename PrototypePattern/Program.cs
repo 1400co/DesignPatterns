1b61a3a7376e7632769a89099836192459940f69
Oscar Rueda <alonso@leapfactor.com>
2018-12-13 12:59:56 -0500

    PrototypePattern.csproj added
    Signed-off-by: Oscar Rueda <alonso@leapfactor.com>

---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Method Pattern example by 1400.");
+
+            Developer dev = new Developer();
+            dev.Name = "Rahul";
+            dev.Role = "Team Leader";
+            dev.PreferredLanguage = "C#";
+
+            Developer devCopy = (Developer)dev.Clone();
+            devCopy.Name = "Arif"; //Not mention Role and PreferredLanguage, it will copy above
+
+            Console.WriteLine(dev.GetDetails());
+            Console.WriteLine(devCopy.GetDetails());
+
+            Typist typist = new Typist();
+            typist.Name = "Monu";
+            typist.Role = "Typist";
+            typist.WordsPerMinute = 120;
+
+            Typist typistCopy = (Typist)typist.Clone();
+            typistCopy.Name = "Sahil";
+            typistCopy.WordsPerMinute = 115;//Not mention Role, it will copy above
+
+            Console.WriteLine(typist.GetDetails());
+            Console.WriteLine(typistCopy.GetDetails());
+
+            Console.ReadKey();
+        }
+    }
+}
