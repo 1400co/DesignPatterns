60c9d77982d453a5e4e5d53b32ffbdc9f3ffc7f4
Oscar Rueda <alonso@leapfactor.com>
2018-12-13 12:34:58 -0500

    SingletonPattern.csproj added
    Signed-off-by: Oscar Rueda <alonso@leapfactor.com>

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Method Pattern example by 1400.");
+
+            Singleton.Instance.Show();
+            Singleton.Instance.Show();
+
+            Console.ReadKey();
+
+        }
+    }
+}
