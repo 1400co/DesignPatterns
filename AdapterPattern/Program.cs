41417763893ac6077e986c0139f90dc22ae795a0
Oscar Rueda <alonso@leapfactor.com>
2019-01-08 09:14:07 -0500

    Adapter Pattern Added
    Signed-off-by: Oscar Rueda <alonso@leapfactor.com>

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget Itarget = new EmployeeAdapter();
+            ThirdPartyBillingSystem client = new ThirdPartyBillingSystem(Itarget);
+            client.ShowEmployeeList();
+
+            Console.ReadKey();
+        }
+    }
+}
